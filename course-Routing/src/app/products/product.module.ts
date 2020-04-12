adc58fd73348afec3fba4c94ea42043dd56e7557
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-04-12 20:36:52 -0300

    Adding Resolver to Route

--- course-Routing/src/app/products/product.module.ts
+++ course-Routing/src/app/products/product.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-
+import { ProductResolver } from './product-resolver.service';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { ProductService } from './product.service';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
       { path: 'products/:id/edit', component: ProductEditComponent }
 
     ])
