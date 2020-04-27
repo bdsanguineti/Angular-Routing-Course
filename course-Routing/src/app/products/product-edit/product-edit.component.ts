bdd0154879c6d33974e5764d4e2869612f4da0c1
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-04-27 19:21:26 -0300

    Disabling Save Button when a requiered Field is empty

--- course-Routing/src/app/products/product-edit/product-edit.component.ts
+++ course-Routing/src/app/products/product-edit/product-edit.component.ts
@@ -15,6 +15,7 @@ export class ProductEditComponent implements OnInit {
   errorMessage: string;
 
   product: Product;
+  private dataIsValid: {[key: string]: boolean} = {};
 
   constructor(private productService: ProductService,
               private messageService: MessageService,
@@ -57,6 +58,15 @@ export class ProductEditComponent implements OnInit {
     }
   }
 
+  isValid(path?: string): boolean{
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+  }
+
   saveProduct(): void {
     if (true === true) {
       if (this.product.id === 0) {
@@ -82,5 +92,27 @@ export class ProductEditComponent implements OnInit {
 
     // Navigate back to the product list
     this.router.navigate(['/products']);
+  }
+
+  validate(): void {
+    // Clear the validation Object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName &&
+    this.product.productName.length >= 3 &&
+    this.product.productCode) {
+      this.dataIsValid['info'] = true;
+      } else {
+        this.dataIsValid['info'] = false;
+      }
+
+    // 'tags' tab
+    if (this.product.category &&
+    this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true;
+    } else {
+      this.dataIsValid['tags'] = false;
+    }
   }
 }
