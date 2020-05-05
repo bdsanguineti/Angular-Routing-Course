b3f5746a4c6ac12d5890ce1209a91936b279bad0
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-05-05 00:26:58 -0300

    Adding Guard Again now that we have Lazy Loading Working

--- course-Routing/src/app/app-routing.module.ts
+++ course-Routing/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
 
 const Rutas = [
   { path: 'welcome', component: WelcomeComponent },
   {
     path: 'products',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./products/product.module').then(m => m.ProductModule)
   },
